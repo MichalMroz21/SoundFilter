2a55cf715b4877c0694a755e16b95adac2d8575f
MichalMroz <125133223+MichalMroz21@users.noreply.github.com>
2025-03-27 18:19:21 +0100

    removed git repo

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,43 @@
+import type { Metadata } from "next";
+import { ThemeProvider } from "@/components/theme-provider";
+import { Lato } from 'next/font/google'; 
+import { cn } from "@/lib/utils";
+import "./globals.css";
+import Navbar from "@/components/navbar";
+
+export const metadata: Metadata = {
+  title: "SoundFilter",
+  description: "App for filtering sounds",
+};
+
+const lato = Lato({
+  weight: ['400', '700'], 
+  subsets: ['latin'], 
+  display: 'swap', 
+});
+
+export default function RootLayout({ children, }: Readonly<{ children: React.ReactNode; }>) {
+  return (
+    <>
+      <html lang="en" suppressHydrationWarning>
+        <head />
+        <body className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          lato.className
+        )}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >           
+            <Navbar className="h-[70px] sticky top-0"/>
+            <main>
+              {children}
+            </main>     
+          </ThemeProvider>
+        </body>
+      </html>
+    </>
+  );
+}
---
+++ frontend/components/logo.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import Link from "next/link"
+
+export default function Logo() {
+    return (
+        <Link href={"/"}>
+            <img src="/images/logo.png" alt="soundfilter logo" className="w-10 h-10 rounded-md"></img>
+        </Link>
+    );
+}
