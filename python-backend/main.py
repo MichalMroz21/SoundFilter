ed687d2ff08d38026c656cad47657f952be3bec5
MichalMroz <125133223+MichalMroz21@users.noreply.github.com>
2025-03-29 22:30:51 +0100

    added api comment

--- python-backend/main.py
+++ python-backend/main.py
@@ -2,6 +2,7 @@ import uvicorn
 from fastapi import FastAPI
 app = FastAPI()
 
+#DO ALL ENTRYPOINTS AS "audio-api" ... slash something...
 @app.get("/audio-api/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
